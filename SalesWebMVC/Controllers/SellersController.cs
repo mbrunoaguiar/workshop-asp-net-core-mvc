f31c103dfff1bc94203776d620b170cfb1987101
Bruno Aguiar <mbruno.aguiar@gmail.com>
2018-10-19 11:02:18 -0300

    Criando um formulario manualmente

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
@@ -19,10 +20,22 @@ namespace SalesWebMVC.Controllers
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
-
             return View(list);
-                        
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        
     }
 }
